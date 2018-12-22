f6f57229b024011c6d08ded2d21393a0db019844
Nick Bertrand <nickbertrand1@gmail.com>
2018-12-21 21:06:00 -0600

    Adding index files to make imports easier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './containers/Home/Home';
-import Resume from './containers/Resume/Resume';
-import Projects from './containers/Projects/Projects';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Home } from './containers/Home';
+import { Resume } from './containers/Resume';
+import { Projects } from './containers/Projects';
 
 
 class App extends Component {
