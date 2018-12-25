3a7fb0319b5920625e79cde1228059fe5880baa2
Nicholas Bertrand <nickbertrand1@gmail.com>
2018-12-25 17:02:38 -0600

    Removing main component because it was basically useless...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 import styled from 'styled-components/macro'
-import { Main } from './components/Main';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Home } from './containers/Home';
+import { Resume } from './containers/Resume';
+import { Projects } from './containers/Projects';
+
 
 
 class App extends Component {
@@ -16,7 +19,11 @@ class App extends Component {
       <BrowserRouter>
         <AppContainer>
           <Header/>
-          <Main/>
+          <main>
+            <Route path="/" exact component={Home}/>
+            <Route path="/resume" component={Resume}/>
+            <Route path="/projects" component={Projects}/>
+          </main>
           <Footer/>
         </AppContainer>
       </BrowserRouter>
